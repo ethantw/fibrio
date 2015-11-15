ebf282e3bd5a72146d79a8b11a8353958ec83a66
Chen Yijun <ethantw@me.com>
2015-11-16 06:02:41 +0800

    Rename `getAggr` ➡️ `aggregate`

--- src/aggregate.js
+++ src/aggregate.js
@@ -1,29 +1,32 @@
 
 import Finder from './core'
+import { type, first, next } from './fn/manipulate'
 
 Object.assign( Finder.fn, {
   /**
    * Get the text aggregate of a node w/o resorting to
    * `$node.text()`
    */
-  getAggr( node=this.context ) {
-    if ( node.type === 'text' )  return [ node.data ]
+  aggregate( node=this.context ) {
+    // Found the text and return it.
+    if ( node::type() === 'text' )  return [ node.data ]
+
+    // Exclude unwanted elements.
     if ( this.filterFn && !this.filterFn( node ))  return []
 
     let ret = [ '' ]
     let i    = 0
-    let first = typeof node.children === 'function' ? node.children()[0] : node.children[0]
 
-    if ( node = first ) do {
-      if ( node.type === 'text' ) {
+    if ( node = node::first()) do {
+      if ( node::type() === 'text' ) {
         ret[ i ] += node.data
         continue
       }
 
-      let innerText = this.getAggr( node )
+      let innerText = this.aggregate( node )
 
       if (
-        node.type === 'tag' &&
+        node::type() === 'tag' &&
         this.boundaryFn && this.boundaryFn( node )
       ) {
         ret[ ++i ] = innerText
@@ -37,7 +40,7 @@ Object.assign( Finder.fn, {
           ret[ ++i ] = ''
         }
       }
-    } while ( node = node.next )
+    } while ( node = node::next())
     return ret
   },
 })
