8f127ab0d0c4a2d761da449147ee0f59a5cb39d4
Chen Yijun <ethantw@me.com>
2015-11-17 19:43:44 +0800

    Minor adjustments

--- src/prop.js
+++ src/prop.js
@@ -2,11 +2,10 @@
 import Finder from './core'
 
 Object.assign( Finder.fn, {
-  version:  '@VERSION',
-  preset:   'prose',
-  mode:     'retain',
-  context:  undefined,
-  phase:    [],
+  version:     '@VERSION',
+  portionMode: 'retain',
+  context:     undefined,
+  phase:       [],
   selector: {
     avoid: new Set( Finder.ELMT.NON_TEXT ),
     bdry:  new Set( Finder.ELMT.BDRY ),
