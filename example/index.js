31c250c81f250e4583dc5fa6b92598254b22de71
Chen Yijun <ethantw@me.com>
2015-12-13 01:58:08 +0800

    Load HTML file(s) asynchronously

--- example/index.js
+++ example/index.js
@@ -1,86 +1,46 @@
 
 'use strict'
 
-// const Fibrio = require( 'fibrio' )
+const fs     = require( 'fs' )
 const Fibrio = require( '..' )
 
+const log   = console.log
+const url   = url => `${process.cwd()}/example/${url}`
+const start = Date.now()
+
 console.time( 'Fibrio' )
+fs.readFile(
+  url( 'index.html' ),
+  'utf-8',
+  ( err, html ) => {
+    let fib = Fibrio( html )
 
-let fib = Fibrio( `
-<!-- LOL LOOOOL LOL -->
-L<i>OL</i> w Lol x LoooooL y LOL z lOl
-<p>L<i>OOooO</i>L w Lol x LoL y LOL z lOl</p>
-<body>
-<div>
-  lool
-  looo<a>OOl</a>
-  <p>xxx lo, ol</p>
-  <p>lol lol LoooOOL lO<u>OO</u>ool loOOooL</p>
-  <p>LooOoLing</p>
-  <p class="x">1. <i>LOL</i>, mean<i>ing</i> laughing out loud.</p>
-  <p>2. LOL, meaning laughing out loud.</p>
-  <p>3. XXX LOL <dfn>L<i>O</i>l</dfn>, meaning laughing out <em>loud</em>.</p>
-  <p>4. lL<b>O</b>Ll</p>
-  <p>5. Ll<b>Ol</b>L</p>
-  <p>6. xl<b>O</b>l;</p>
-  <pre>l   l<code> lol   LOL </code></pre>
+    fib
+    .qsa( 'body > div' )
+    .action({
+      find:    /lo+l/gi,
+      wrap:    'u',
+      replace: '($&)',
+    })
+    .addBdry( '.ignore' )
+    .process()
 
-  <q><i>x<b>xx</b>xx</i>x<em>yyy</em><b>xxx</b><i>xxxxx</i></q>
-  <p>She play<em>ed</em> him.</p>
-</div>
-<i>He play<em>ed</em> yyy and said LOL.</i>
-text in body...lol
-<div>
-  xxx
-  <p>ooo
-  <article>
-    <p>1. LoO<a class="ignore">OOOooOoL</a>
-    <p>2. LOOOOL
-    <p>3. <a>Looo<b>oooOOo</b>oooL</a>
-    <p>4. <a>Looo<b>oooOOo</b></a>oooL
-  </article>
-</div>
+    const control = fib.html
 
-<style>
-// loling
-p {  color: #333;  }
-</style>
+    fib
+    .wrap( /\b[\w]+(?:ing|ed)\b/gi, 'u' )
+    .removeBdry( 'a' )
+    .wrap( /foobar/gi, 'match' )
 
-<div>
-foo<small>ba<i>r</i></small>
-foo<small>b<i>a<b>r</b></i></small>
-<em>fooba</em>r
-foo<strong>ba</strong>r
-foo<sup>bar</sup>
-foo<acronym>bar</acronym>
-<abbr>fo</abbr>ob<u>a<b>r</b></u>
-</div>
+    const end = Date.now()
+    log( fib.render())
 
-</body>
-` )
+    console.time( 'Revert' )
+    fib.revert( 2 ).render()
 
-
-fib
-.qsa( 'body > div' )
-//.mode( 'retain' )
-.action({
-  find:    /lo+l/gi,
-  wrap:    'u',
-  replace: '($&)',
-})
-.addBdry( '.ignore' )
-.process()
-.wrap( /\b[\w]+(?:ing|ed)\b/gi, 'u' )
-.removeBdry( 'a' )
-.wrap( /foobar/gi, 'match' )
-
-console.log( fib.render())
-//console.log( fib.text )
-console.timeEnd( 'Fibrio' )
-
-console.time( 'Revert' )
-console.log( 'Phase: ', fib.phase.length )
-console.log( fib.revert( 2 ).render() )
-
-console.timeEnd( 'Revert' )
+    log( `\n\nTotal time using Fibrio: ${end - start}ms` )
+    console.timeEnd( 'Revert' )
+    log( `Reversion went properly: ${ fib.html === control }` )
+  }
+)
 
